611ad34d15f965c536e89a0b529a1374297b5c15
Chris Canipe <chris.canipe@wsj.com>
2014-04-10 07:51:54 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+/**
+ * @author Chris Canipe
+ */
+
+
+var video = "<iframe width=560' height='315' src='http://www.youtube.com/embed/o2-RXqU4Lg8?html5=1' frameborder='0' allowfullscreen></iframe>";
+
+
+$(document).ready(function() {
+	setNav();
+});
+
+
+function setNav() {
+	
+	$(".cyclone").on("click", function() {
+		$('#myModal').modal();
+		$(".modal-body").html(video);
+	});	
+	
+	$(".close").on("click", function() {
+		$(".modal-body").empty();
+	});	
+
+
+}
