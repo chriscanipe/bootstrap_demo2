e4e5186217b6dde0401cc134fa250d8efb8ef2bd
Chris Canipe <chris.canipe@wsj.com>
2014-04-10 20:55:36 -0400

    added video stop playing fix to script file

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
  * @author Chris Canipe
  */
 
-
+//This si the iFrame markup for our YouTube video.
+//We got it by selecting the "share" option on the YouTube page...
+//and copy and pasting the embed code. 
 var video = "<iframe width=560' height='315' src='http://www.youtube.com/embed/o2-RXqU4Lg8?html5=1' frameborder='0' allowfullscreen></iframe>";
 
-
+//Document ready. When the page laods, set up our navigation
 $(document).ready(function() {
 	setNav();
 });
@@ -13,14 +15,17 @@ $(document).ready(function() {
 
 function setNav() {
 	
+	//This event listener fires when ".cyclone" button is clicked.
+	//It opens our modal window and inserts the video into the ".modal-body" contaienr
 	$(".cyclone").on("click", function() {
 		$('#myModal').modal();
 		$(".modal-body").html(video);
 	});	
 	
-	$(".close").on("click", function() {
-		$(".modal-body").empty();
-	});	
-
-
+	//This event listener fires when the modal window is closed
+	//and removes the "src" attribute from the iframe, thus killing the video
+	$('#myModal').on('hidden.bs.modal', function () {
+		$('#myModal iframe').removeAttr('src');
+	});
+	
 }
